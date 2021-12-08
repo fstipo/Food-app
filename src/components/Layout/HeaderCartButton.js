7a89540cd108c89c8aa5e3ef7dfd2b49c9c5957f
fstipo <fstipanov@gmail.com>
2021-12-08 07:40:21 +0100

    Fresh start

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,1 @@
-import classes from './HeaderCartButton.module.css';
-import ChartIcon from '../Cart/ChartIcon';
-
-const HeaderCartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span className={classes.icon}>
-        <ChartIcon />
-      </span>
-      <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
-    </button>
-  );
-};
-
-export default HeaderCartButton;
+import classes from 'HeaderCartButton.module.css';
