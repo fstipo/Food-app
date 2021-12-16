7e2f9f15a2be69963a402c022881ff96de9af938
fstipo <fstipanov@gmail.com>
2021-12-16 09:56:43 +0100

    Meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import styles from './AvailableMeals.module.css';
 const DUMMY_MEALS = [
   {
     id: 'm1',
@@ -24,3 +25,14 @@ const DUMMY_MEALS = [
     price: 18.99,
   },
 ];
+
+const AvailableMeals = () => {
+  const mealList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  return (
+    <section className={styles.meals}>
+      <ul>{mealList}</ul>
+    </section>
+  );
+};
+
+export default AvailableMeals;
