6f93dc5f7eecbc034213cfdab1c0202ea97a7473
fstipo <fstipanov@gmail.com>
2022-01-10 10:06:59 +0100

    Header component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import Header from './components/Layout/Header';
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
