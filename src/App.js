e6c017e380e5ae4606c336d814b9836f44421e46
fstipo <fstipanov@gmail.com>
2022-01-10 12:51:00 +0100

    Cart component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <main>
         <Meals />
       </main>
+      <Cart />
     </>
   );
 };
