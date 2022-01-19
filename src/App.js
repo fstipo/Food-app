a593f18cacd778c0aabe988b40a60edb330d96af
fstipo <fstipanov@gmail.com>
2022-01-19 17:10:29 +0100

    Context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 const App = () => {
   const [cartIsShown, setCartIsShown] = useState(false);
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,21 @@
+import React, { useContext } from 'react';
 import ChartIcon from '../Cart/ChartIcon';
 import styles from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce(
+    (curNumber, item) => curNumber + item.amount,
+    0
+  );
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <ChartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
