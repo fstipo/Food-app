2ed39f6bc01393c758b8524a5342c741592c687a
fstipo <fstipanov@gmail.com>
2022-01-25 16:42:45 +0100

    add item better version

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,11 +7,26 @@ const defaultCartState = {
 };
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
-    console.log(updatedItems);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
-    console.log(updatedTotalAmount);
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
 
     return {
       items: updatedItems,
