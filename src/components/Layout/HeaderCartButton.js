28d7ef84051ace45720869f6e0413f33eaee11ca
fstipo <fstipanov@gmail.com>
2022-01-29 12:52:46 +0100

    add effect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ChartIcon from '../Cart/ChartIcon';
 import styles from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const [btnEffect, setBtnEffect] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce(
+  const numberOfCartItems = items.reduce(
     (curNumber, item) => curNumber + item.amount,
     0
   );
 
+  const btnClasses = `${styles.button} ${btnEffect ? styles.bump : ' '}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnEffect(true);
+    const timer = setTimeout(() => {
+      setBtnEffect(false);
+    }, 300);
+
+    // cleanUp function
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <ChartIcon />
       </span>
